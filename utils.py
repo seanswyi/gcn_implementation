e8086086cf855c21c9bbc160254ce9fd66109c1a
Sean Yi / 이석원 <seantuokc@gmail.com>
2020-03-07 15:23:51 +0900

    Added in functions to create adjacency and degree matrices.

--- utils.py
+++ utils.py
@@ -16,3 +16,21 @@ def one_hot_enc(labels):
         template[idx][classes[label]] = 1
 
     return template
+
+
+def create_adj_mat(nodes, edges):
+    adj_mat = np.zeros(shape=(nodes.shape[0], nodes.shape[0]))
+
+    for _, pair in enumerate(edges):
+        adj_mat[pair[0], pair[1]] = 1
+
+    return adj_mat
+
+
+def create_deg_mat(adj_mat):
+    deg_mat = np.zeros(shape=adj_mat.shape)
+
+    for idx, row in enumerate(adj_mat):
+        deg_mat[idx, idx] = row.sum()
+
+    return deg_mat
