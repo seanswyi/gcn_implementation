b54569452bc8be9cb03b80eb7d48c6157f691ae9
Sean Yi / 이석원 <seantuokc@gmail.com>
2020-03-07 23:02:14 +0900

    Changed labels to be appropriate for cross entropy loss. Also set up indices.

--- dataset.py
+++ dataset.py
@@ -1,8 +1,10 @@
 import os
+import pdb
 
 import numpy as np
+import torch
 
-from utils import create_adj_mat, create_deg_mat, one_hot_enc
+from utils import create_adj_mat, create_adj_hat, one_hot_enc
 
 
 class Data():
@@ -18,7 +20,7 @@ class Data():
         edges = np.genfromtxt(fname=self.edges_path, dtype=np.int)
 
         idxs = np.array(nodes[:, 0], dtype=np.int)
-        idxs_features = nodes[:, :-1]
+        idxs_features = np.array(nodes[:, :-1], dtype=np.float32)
         labels = nodes[:, -1]
 
         idx_ordered = {og: new for new, og in enumerate(idxs)}
@@ -26,14 +28,27 @@ class Data():
         for idx, row in enumerate(idxs_features):
             idxs_features[idx][0] = idx_ordered[row[0]]
 
-        idxs = np.array(idxs_features[:, 0], dtype=np.int32)
-        features = np.array(idxs_features[:, 1:], dtype=np.float32)
+        idxs = np.array(idxs_features[:, 0], dtype=np.int)
+        features = np.array(idxs_features[:, 1:], dtype=np.float)
         labels = one_hot_enc(labels)
 
         for idx, pair in enumerate(edges):
             edges[idx] = [idx_ordered[pair[0]], idx_ordered[pair[1]]]
 
         adj_mat = create_adj_mat(nodes, edges)
-        adj_mat_tilde = adj_mat + np.eye(N=adj_mat.shape[0])
+        adj_hat = create_adj_hat(adj_mat)
 
-        deg_mat = create_deg_mat(adj_mat_tilde)
+        self.adj_hat = torch.tensor(adj_hat)
+        self.features = torch.tensor(features)
+        self.edges = torch.tensor(edges)
+        self.labels = torch.tensor(np.where(labels)[1])
+
+        self.idx_train = torch.tensor(range(140))
+        self.idx_valid = torch.tensor(range(200, 500))
+        self.idx_test = torch.tensor(range(500, 1500))
+
+        if torch.cuda.is_available():
+            self.adj_hat = self.adj_hat.to('cuda')
+            self.features = self.features.to('cuda')
+            self.edges = self.edges.to('cuda')
+            self.labels = self.labels.to('cuda')
