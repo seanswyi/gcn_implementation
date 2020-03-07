ebfacbafc98458bb8680df5dbfe45c1dfc2a4354
Sean Yi / 이석원 <seantuokc@gmail.com>
2020-03-07 14:52:43 +0900

    Added in basic arguments.

---
+++ config.py
@@ -0,0 +1,9 @@
+import argparse
+
+
+def get_args():
+    argp = argparse.ArgumentParser(description='GCN',
+                                   formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    # Data stuff.
+    argp.add_argument('--data_dir', type=str, default='../graph_data')
