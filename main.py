c79f02b99aaf43a6cea6ab8a84cce381187abcc2
Sean Yi / 이석원 <seantuokc@gmail.com>
2020-03-07 23:02:25 +0900

    Added in the name = main thing.

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
+import pdb
+
 from config import get_args
 from dataset import Data
+from models import GCN
 
 
 def main():
     config = get_args()
     dataset = Data(config)
+
+    adj_hat = dataset.adj_hat
+    features= dataset.features
+    labels = dataset.labels
+
+    model = GCN(config=config, num_features=features.shape[1], num_classes=labels.shape[1])
+
+    pdb.set_trace()
+
+
+if __name__ == '__main__':
+    main()
