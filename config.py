20246315a6e5bc497b21f49d848269c4c1ee3500
Sean Yi / 이석원 <seantuokc@gmail.com>
2020-03-08 14:48:05 +0900

    Added arguments in for dropout rate.

--- config.py
+++ config.py
@@ -10,5 +10,11 @@ def get_args():
 
     # Model stuff.
     argp.add_argument('--num_hidden', type=int, default=16)
+    argp.add_argument('--dropout_rate', type=float, default=0.5)
+
+    # Model running stuff.
+    argp.add_argument('--num_epochs', type=int, default=200)
+    argp.add_argument('--lr', type=float, default=0.01)
+    argp.add_argument('--optimizer', type=str, default='adam')
 
     return argp.parse_args()
