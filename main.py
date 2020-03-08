e054b20c9a0c8dae75600a7c65893eeb8251525b
Sean Yi / 이석원 <seantuokc@gmail.com>
2020-03-08 14:48:05 +0900

    Added in model and solver calling.

--- main.py
+++ main.py
@@ -1,19 +1,27 @@
 import pdb
 
+import torch
+
 from config import get_args
 from dataset import Data
 from models import GCN
+from solver import Trainer
 
 
 def main():
     config = get_args()
     dataset = Data(config)
 
-    adj_hat = dataset.adj_hat
-    features= dataset.features
-    labels = dataset.labels
+    num_features = dataset.features.shape[1]
+    num_classes = dataset.labels.max().item() + 1
 
-    model = GCN(config=config, num_features=features.shape[1], num_classes=labels.shape[1])
+    model = GCN(config=config, num_features=num_features, num_classes=num_classes)
+    trainer = Trainer(config, model, dataset)
+
+    if torch.cuda.is_available():
+        model = model.to('cuda')
+
+    trainer.train()
 
     pdb.set_trace()
 
