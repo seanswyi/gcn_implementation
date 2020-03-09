93f970269b9bd5a2d5f5c0be00c24889f82778e3
Sean Yi <seantuokc@gmail.com>
2020-03-09 13:03:53 +0900

    Added in code for accuracy.

--- utils.py
+++ utils.py
@@ -4,30 +4,12 @@ import numpy as np
 import torch
 
 
-def one_hot_enc(labels):
-    """
-    Simple one hot encoder for labels.
+def accuracy(pred, labels):
+    pred_idxs = torch.argmax(pred, dim=1)
+    num_correct = (pred_idxs == labels).sum().item()
+    acc = num_correct / len(labels)
 
-    We first construct a dictionary that maps labels to unique indices.
-    We then construct a template of zeros and change the appropriate indices
-      to 1.
-    """
-    classes = {label: idx for idx, label in enumerate(set(labels))}
-    template = np.zeros(shape=(labels.shape[0], len(classes)))
-
-    for idx, label in enumerate(labels):
-        template[idx][classes[label]] = 1
-
-    return template
-
-
-def create_adj_mat(nodes, edges):
-    adj_mat = np.zeros(shape=(nodes.shape[0], nodes.shape[0]))
-
-    for _, pair in enumerate(edges):
-        adj_mat[pair[0], pair[1]] = 1
-
-    return adj_mat
+    return acc
 
 
 def create_adj_hat(adj_mat):
@@ -37,6 +19,15 @@ def create_adj_hat(adj_mat):
     adj_hat = np.matmul(np.matmul(deg_tilde, adj_tilde), deg_tilde)
 
     return adj_hat
+
+
+def create_adj_mat(nodes, edges):
+    adj_mat = np.zeros(shape=(nodes.shape[0], nodes.shape[0]))
+
+    for _, pair in enumerate(edges):
+        adj_mat[pair[0], pair[1]] = 1
+
+    return adj_mat
 
 
 def create_deg_mat(adj_mat):
@@ -56,3 +47,20 @@ def get_optimizer(config, model):
         optimizer = torch.optim.Adam(params=params, lr=lr)
 
     return optimizer
+
+
+def one_hot_enc(labels):
+    """
+    Simple one hot encoder for labels.
+
+    We first construct a dictionary that maps labels to unique indices.
+    We then construct a template of zeros and change the appropriate indices
+      to 1.
+    """
+    classes = {label: idx for idx, label in enumerate(set(labels))}
+    template = np.zeros(shape=(labels.shape[0], len(classes)))
+
+    for idx, label in enumerate(labels):
+        template[idx][classes[label]] = 1
+
+    return template
